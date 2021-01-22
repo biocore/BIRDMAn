0acd386d21b43b000b16ad3b31318301429b8d10
Gibraan Rahman <grahman@eng.ucsd.edu>
2021-01-22 13:39:44 -0600

    remove qiime dependency update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,31 +1,14 @@
 import biom
-import numpy as np
 import pandas as pd
 import pickle
 import pytest
 
 from patsy import dmatrix
-from qiime2 import Artifact
 
 
 def example_table():
-    table = Artifact.load("tests/data/table-deblur.qza")
-    table_df = table.view(pd.DataFrame)
-
-    np.random.seed(42)
-    random_samples = table_df.sample(20).index
-    random_features = table_df.T.sample(30).index
-    table_filt = table_df.loc[random_samples, random_features]
-
-    sample_sums = table_filt.sum(axis=1)
-    feature_sums = table_filt.sum(axis=0)
-    random_samples = sample_sums[sample_sums > 0].index.tolist()
-    random_features = feature_sums[feature_sums > 0].index.tolist()
-
-    table_filt = table_filt.loc[random_samples, :]
-    table_filt = table_filt.loc[:, random_features]
-
-    return table_filt
+    table_biom = biom.load_table("tests/data/table-deblur.biom")
+    return table_biom.to_dataframe().T
 
 
 def example_metadata():
@@ -40,13 +23,7 @@ def example_metadata():
 
 @pytest.fixture
 def table_biom():
-    table_filt = example_table()
-
-    table_biom = biom.table.Table(
-        table_filt.T.values,
-        table_filt.columns,
-        table_filt.samples,
-    )
+    table_biom = biom.load_table("tests/data/table-deblur.biom")
     return table_biom
 
 
