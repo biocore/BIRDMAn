65a2b09e3e3d0033a8c16000b5c7cf56422920ad
Gibraan Rahman <grahman@eng.ucsd.edu>
2021-02-02 09:38:14 -0600

    add test for converting beta coordinates

--- birdman/util.py
+++ birdman/util.py
@@ -1,26 +1,20 @@
 import numpy as np
 
 
-# TODO: Make sure 1-D array works
 def alr_to_clr(x: np.ndarray) -> np.ndarray:
     """Convert ALR coordinates to centered CLR coordinates.
 
     Parameters:
     -----------
     x: np.ndarray
-        matrix of ALR coordinates (feature x draw) OR (1 x draw)
+        matrix of ALR coordinates (features x draws)
 
     Returns:
     --------
     np.ndarray
         centered CLR coordinates
     """
-    if x.ndim == 2:  # matrix of parameter draws
-        num_draws = x.shape[1]
-    elif x.ndim == 1:  # vector of parameter draws
-        num_draws = 1
-    else:
-        raise ValueError("ALR coordinates must be matrix or vector!")
+    num_draws = x.shape[1]
     z = np.zeros((1, num_draws))
     x_clr = np.vstack((z, x))
     x_clr = x_clr - x_clr.mean(axis=0).reshape(1, -1)
@@ -37,11 +31,11 @@ def convert_beta_coordinates(beta: np.ndarray) -> np.ndarray:
 
     Returns:
     --------
-    beta_clr: np.ndarray
+    np.ndarray
         beta CLR coefficients (p covariates x (d+1) features x n draws)
     """
     num_covariates, num_features, num_draws = beta.shape
     beta_clr = np.zeros((num_covariates, num_features+1, num_draws))
-    for i in range(num_covariates):
+    for i in range(num_covariates):  # TODO: vectorize
         beta_clr[i, :, :] = alr_to_clr(beta[i, :, :])
     return beta_clr
--- tests/test_util.py
+++ tests/test_util.py
@@ -18,3 +18,20 @@ def test_alr_to_clr():
     exp_clr = clr(mat.T)                    # 5 x 4
 
     np.testing.assert_array_almost_equal(clr_mat, exp_clr)
+
+
+def test_convert_beta_coordinates():
+    # Total: (p covariates x d features x n draws)
+    # Each draw: (p covariates x d features)
+    draw1 = np.array([
+        [0.1, 0.2, 0.3, 0.4],
+        [0.3, 0.1, 0.1, 0.5]
+    ])
+    draw2 = np.array([
+        [0.2, 0.2, 0.3, 0.3],
+        [0.1, 0.6, 0.2, 0.1]
+    ])
+    alr_coords = np.dstack([alr(draw1), alr(draw2)])  # 2 x 3 x 2
+    clr_coords = util.convert_beta_coordinates(alr_coords)
+    exp_coords = np.dstack([clr(draw1), clr(draw2)])
+    np.testing.assert_array_almost_equal(clr_coords, exp_coords)
