743612b908047c25233317c7bef00242e8223cef
Gibraan Rahman <grahman@eng.ucsd.edu>
2021-03-23 12:21:32 -0700

    trying nb lme

--- birdman/default_models.py
+++ birdman/default_models.py
@@ -10,7 +10,8 @@ TEMPLATES = resource_filename("birdman", "templates")
 DEFAULT_MODEL_DICT = {
     "negative_binomial": {
         "chains": os.path.join(TEMPLATES, "negative_binomial.stan"),
-        "features": os.path.join(TEMPLATES, "negative_binomial_single.stan")
+        "features": os.path.join(TEMPLATES, "negative_binomial_single.stan"),
+        "lme": os.path.join(TEMPLATES, "negative_binomial_lme.stan")
     },
     "multinomial": os.path.join(TEMPLATES, "multinomial.stan")
 }
@@ -87,6 +88,94 @@ class NegativeBinomial(Model):
         param_dict = {
             "B_p": beta_prior,
             "phi_s": cauchy_scale
+        }
+        self.add_parameters(param_dict)
+
+
+class NegativeBinomialLME(Model):
+    """Fit count data using negative binomial model considering subject as
+    a random effect.
+
+    .. math::
+
+        y_{ij} &\\sim \\textrm{NB}(\\mu_{ij},\\phi_j)
+
+        \\mu_{ij} &= n_i \\times p_{ij}
+
+        \\textrm{alr}^{-1}(p_i) &= x_i \\cdot \\beta + z_i \\cdot u
+
+    Priors:
+
+    .. math::
+
+        \\beta_j &\\sim \\textrm{Normal}(0, B_p), B_p \\in \\mathbb{R}_{>0}
+
+        \\frac{1}{\\phi_j} &\\sim \\textrm{Cauchy}(0, C_s), C_s \\in
+            \\mathbb{R}_{>0}
+
+        \\u_{\\textrm{subj}} &\\sim \\textrm{Normal}(0, u_p), u_p \\in
+            \\mathbb{R}_{>0}
+
+
+    :param table: Feature table (features x samples)
+    :type table: biom.table.Table
+
+    :param formula: Design formula to use in model
+    :type formula: str
+
+    :param group_var: Variable in metadata to use as grouping
+    :type group_var: str
+
+    :param metadata: Metadata for design matrix
+    :type metadata: pd.DataFrame
+
+    :param num_iter: Number of posterior draws (used for both warmup and
+        sampling), defaults to 500
+    :type num_iter: int
+
+    :param chains: Number of chains to use in MCMC, defaults to 4
+    :type chains: int
+
+    :param seed: Random seed to use for sampling, defaults to 42
+    :type seed: float
+
+    :param beta_prior: Standard deviation for normally distributed prior values
+        of beta, defaults to 5.0
+    :type beta_prior: float
+
+    :param cauchy_scale: Scale parameter for half-Cauchy distributed prior
+        values of phi, defaults to 5.0
+    :type cauchy_scale: float
+
+    :param group_var_prior: Standard deviation for normally distributed prior
+        prior values of group_var, defaults to 1.0
+    :type group_var_prior: float
+    """
+    def __init__(
+        self,
+        table: biom.table.Table,
+        formula: str,
+        group_var: str,
+        metadata: pd.DataFrame,
+        num_iter: int = 500,
+        chains: int = 4,
+        seed: float = 42,
+        beta_prior: float = 5.0,
+        cauchy_scale: float = 5.0,
+        group_var_prior: float = 1.0
+    ):
+        filepath = DEFAULT_MODEL_DICT["negative_binomial"]["lme"]
+        super().__init__(table, formula, metadata, filepath, num_iter, chains,
+                         seed, parallelize_across="chains")
+        group_var_series = metadata[group_var].loc[self.sample_names]
+        samp_subj_map = pd.get_dummies(group_var_series)
+
+        param_dict = {
+            "B_p": beta_prior,
+            "phi_s": cauchy_scale,
+            "S": len(group_var_series.unique()),
+            "Z": samp_subj_map.values,
+            "u_p": group_var_prior
         }
         self.add_parameters(param_dict)
 
---
+++ birdman/templates/negative_binomial_lme.stan
@@ -0,0 +1,66 @@
+data {
+  int<lower=0> N;       // number of sample IDs
+  int<lower=0> S;       // number of groups (subjects)
+  int<lower=0> D;       // number of dimensions
+  int<lower=0> p;       // number of covariates
+  real depth[N];        // sequencing depths of microbes
+  matrix[N, p] x;       // covariate matrix
+  matrix[N, S] Z;       // mapping of sample to subject
+  int y[N, D];          // observed microbe abundances
+  real<lower=0> B_p;    // stdev for covariate Beta Normal prior
+  real<lower=0> phi_s;  // scale for dispersion Cauchy prior
+  real<lower=0> u_p;    // stdev for subject intercept Normal prior
+}
+
+parameters {
+  matrix[p, D-1] beta;
+  vector<lower=0>[D] reciprocal_phi;
+  vector<lower=0>[S] subj_int;
+}
+
+transformed parameters {
+  matrix[N, D-1] lam;
+  matrix[N, D] lam_clr;
+  vector<lower=0>[D] phi;
+
+  for (i in 1:D){
+    phi[i] = 1. / reciprocal_phi[i];
+  }
+
+  lam = x*beta + Z*subj_int;
+  lam_clr = append_col(to_vector(rep_array(0, N)), lam);
+}
+
+model {
+  // setting priors ...
+  for (i in 1:D){
+    reciprocal_phi[i] ~ cauchy(0., phi_s);
+  }
+  for (i in 1:D-1){
+    for (j in 1:p){
+      beta[j, i] ~ normal(0., B_p); // uninformed prior
+    }
+  }
+  for (i in 1:S){
+    subj_int[i] ~ normal(0., u_p);
+  }
+
+  // generating counts
+  for (n in 1:N){
+    for (i in 1:D){
+      target += neg_binomial_2_log_lpmf(y[n, i] | depth[n] + lam_clr[n, i], phi[i]);
+    }
+  }
+}
+
+generated quantities {
+  matrix[N, D] y_predict;
+  matrix[N, D] log_lik;
+
+  for (n in 1:N){
+    for (i in 1:D){
+      y_predict[n, i] = neg_binomial_2_log_rng(depth[n] + lam_clr[n, i], phi[i]);
+      log_lik[n, i] = neg_binomial_2_log_lpmf(y[n, i] | depth[n] + lam_clr[n, i], phi[i]);
+    }
+  }
+}
