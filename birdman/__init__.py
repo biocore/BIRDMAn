67f0e188b599d6bbbea7b3879eb6b6d341fafdd2
Gibraan Rahman <grahman@eng.ucsd.edu>
2022-07-22 13:03:10 -0700

    Update to v0.1.0

--- birdman/__init__.py
+++ birdman/__init__.py
@@ -3,6 +3,8 @@ from .model_base import (BaseModel, TableModel, SingleFeatureModel,
 from .default_models import (NegativeBinomial, NegativeBinomialLME,
                              NegativeBinomialSingle)
 
+__version__ = "0.1.0"
+
 __all__ = ["BaseModel", "TableModel", "SingleFeatureModel", "ModelIterator",
            "NegativeBinomial", "NegativeBinomialSingle",
            "NegativeBinomialLME"]
